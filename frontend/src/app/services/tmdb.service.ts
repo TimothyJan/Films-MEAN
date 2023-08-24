c60fe1f524af14680fa05109e92df29c5b7d19f6
Timothy jan <jan.timothy@gmail.com>
2023-08-24 14:43:57 -0700

    Updated Angular

--- frontend/src/app/services/tmdb.service.ts
+++ frontend/src/app/services/tmdb.service.ts
@@ -29,7 +29,7 @@ export class TmdbService {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        // Authorization: authorizationHeader
+        Authorization: typeof authorizationHeader
       }
     } | undefined;
 
